aefabd33792f6e257f054014036342b596d37293
jin <jin@caixingxing.com>
2023-07-04 18:21:51 +0800

    修改参数

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ app.use(cors())
 app.use('/', createProxyMiddleware({
   target: 'https://yourappname.metered.live',
   changeOrigin: true,
-  cookieDomainRewrite: 'yourappname.metered.live',
-  headers: {
-    'Host': 'yourappname.metered.live',
-    'Referer': 'https://yourappname.metered.live',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-  },
   on: {
     proxyReq: (proxyReq, req, res) => {
       console.log(+new Date())
