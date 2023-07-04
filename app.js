ea66e51ed786aee527ac61c9c6112e40e648504f
jin <jin@caixingxing.com>
2023-07-04 17:51:58 +0800

    添加参数

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
-const cors = require('cors');
+const cors = require('cors')
 
 const app = express()
 const port = 9000
@@ -8,8 +8,14 @@ const port = 9000
 app.use(cors())
 
 app.use('/', createProxyMiddleware({
-  target: 'https://yourappname.metered.live', 
+  target: 'https://yourappname.metered.live',
   changeOrigin: true,
+  cookieDomainRewrite: 'yourappname.metered.live',
+  headers: {
+    'Host': 'yourappname.metered.live',
+    'Referer': 'https://yourappname.metered.live',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+  },
   on: {
     proxyReq: (proxyReq, req, res) => {
       console.log(+new Date())
