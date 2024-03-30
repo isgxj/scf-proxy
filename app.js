91f1e7a524aa276b7c39c93e1d02249f01c9c5c0
jin <jin@caixingxing.com>
2024-03-30 11:06:20 +0800

    更新请求头

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/', createProxyMiddleware({
     'Via': '',
     'X-Real-IP': '',
     'X-Forwarded-Host': '',
-    'User-Agent': 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.16(0x1800103e) NetType/WIFI Language/en-us',
     // 'Accept-Encoding': '',
   },
   on: {
